2d70a17b12c3a7d738a2eb55deb1b5ad2696c966
Rana Mohib <rana.mohib.ullah.khan@gmail.com>
2022-10-08 12:29:20 +0500

    Add Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'white': '#ffffff',
+      'navy': '#310D7D',
+      'blue': '#4700B3',
+      'sky': '#5E74FF',
+      'green': '#22DB54',
+      'transparent': 'transparent',
+      'current': 'currentColor'
+    },
+    fontFamily: {
+      sans: ['Codec Pro', 'sans-serif'],
+      arabic: ['Arabic', 'sans-serif']
+    },
   },
   plugins: [],
 }
+
